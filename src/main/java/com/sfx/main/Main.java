32f9f0920221782f8ee1659603b21ebe7636df1b
oym <oym@qq.com>
2019-07-19 23:25:17 +0800

    第一次提交

---
+++ src/main/java/com/sfx/main/Main.java
@@ -0,0 +1,11 @@
+package com.sfx.main;
+
+/**
+ * @author 三分醒
+ * @create 2019-07-19 21:13
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
